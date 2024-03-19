f93288ef85cfe46482416b4edca17307000db780
Rodrigo Martins <rodrigotxt@gmail.com>
2024-03-19 14:45:01 -0300

    add api contacts (to test)

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/contacts', function() {
+    return \App\Models\Contact::all();
+});
