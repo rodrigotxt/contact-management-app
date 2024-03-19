0809213a5e9061d4d6194359b7be35374bb94b8e
Rodrigo Martins <rodrigotxt@gmail.com>
2024-03-19 14:45:18 -0300

    add layout with bootstrap

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Contact Management System - @yield('title')</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
