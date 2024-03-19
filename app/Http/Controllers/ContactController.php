d03446576272c0af397a3ab223af6cf8fc2f3683
Rodrigo Martins <rodrigotxt@gmail.com>
2024-03-19 15:28:54 -0300

    implements ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,6 +8,39 @@ class ContactController extends Controller
 {
     public function index()
     {
-        return view('contact.index')->with('contacts', \App\Models\Contact::all());
+        return view('contacts.index')->with('contacts', \App\Models\Contact::all());
+    }
+
+    public function show($id)
+    {
+        return view('contacts.show')->with('contact', \App\Models\Contact::find($id));
+    }
+
+    public function edit($id)
+    {
+        return view('contacts.edit')->with('contact', \App\Models\Contact::find($id));
+    }
+
+    public function update($id, Request $request)
+    {
+        $contact = \App\Models\Contact::find($id);
+        $contact->update($request->all());
+        return redirect('/contacts/' . $id);
+    }
+
+    public function destroy($id)
+    {
+        \App\Models\Contact::destroy($id);
+        return redirect('/contacts');
+    }
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $contact = \App\Models\Contact::create($request->all());
+        return redirect('/contacts/' . $contact->id);
     }
 }
