c5eb3e7795d72e2726d1d405cb69f63322c28a15
Rodrigo Martins <rodrigotxt@gmail.com>
2024-03-19 15:28:08 -0300

    form contact

---
+++ resources/views/contacts/form.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div>
+    <form action="{{ route('contacts.store') }}" method="POST">
+        @csrf
+        <label for="name">Name:</label>
+        <input class="form-control" type="text" id="name" name="name" required>
+
+        <label for="contact">Contact:</label>
+        <input class="form-control" type="text" id="contact" name="contact" required>
+
+        <label for="contact_email">Email:</label>
+        <input class="form-control" type="email" id="contact_email" name="email" required>
+
+        <div class="py-3">
+            <button class="btn btn-primary mr-5" type="submit">Enviar</button>
+            <a href="{{ route('contacts.index') }}" class="btn btn-outline-secondary ml-3">Cancelar</a>
+        </div>
+
+    </form>
+</div>
+    @endsection
